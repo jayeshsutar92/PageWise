cb2d62200466255815886b53b018190c974ed116
Jayesh Ekanth Sutar <81569410+akajayesh@users.noreply.github.com>
2026-06-27 15:25:20 +0530

    Render Issue

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -8,7 +8,7 @@ from app.db.base import Base
 from app.models.product import Product
 
 config = context.config
-config.set_main_option("sqlalchemy.url", settings.database_url)
+config.set_main_option("sqlalchemy.url", settings.database_url.replace("%", "%%"))
 
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
